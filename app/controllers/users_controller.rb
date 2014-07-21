c16cfe1c923d65412df3d2aabe6729a06e4fbc5a
Sarah Elnagdy <sarah.elnagdy@gmail.com>
2014-07-21 04:48:46 +0200

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user, only: [:edit, :update]
-
+  before_filter :admin_user, only: :destroy
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -45,6 +45,13 @@ class UsersController < ApplicationController
 
   end
 
+  def destroy
+   User.find(params[:id]).destroy
+   flash[:success] = "User destroyed"
+   redirect_to users_path
+
+  end
+
 
   private
   def user_params
@@ -63,6 +70,12 @@ class UsersController < ApplicationController
      @user = User.find(params[:id])
    redirect_to root_path unless current_user?(@user)
      
+   end
+
+
+   def admin_user
+
+   redirect_to root_path unless current_user.admin?
    end
 
   
